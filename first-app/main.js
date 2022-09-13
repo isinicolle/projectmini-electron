eac86c05250bba8f6487db4d5221b7c51679786b
Isis Zapata <isis.zapata.hn@gmail.com>
2022-09-12 23:00:16 -0600

    Update main.js

--- first-app/main.js
+++ first-app/main.js
@@ -1,7 +1,10 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
+//creacion de la vetana principal
 function createWindow () {
+    
+    //tomarse win como variable de la ventana principal
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -10,11 +13,14 @@ function createWindow () {
     }
   })
 
+  //archivo a cargar cuando se cree la ventana
   win.loadFile('index.html')
 }
 
+//cuando la aplicacion este lista se crea la ventana
 app.whenReady().then(() => {
   createWindow()
+
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -23,6 +29,7 @@ app.whenReady().then(() => {
   })
 })
 
+//salir de la aplicacion cuando todas las ventanas esten cerradas
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
